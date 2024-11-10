da60fdac8eec7fbefbbe66b71ba9ce0ccffbe2dd
Allen Yu <allen.yu@wayne.edu>
2024-11-09 19:09:30 -0500

    Initial Commit

---
+++ mvcLearning/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcLearning.Models;
+
+namespace mvcLearning.Controllers
+{
+    public class DemoController : Controller
+    {
+        public static List<Customer> customers = new List<Customer>()
+        {
+            new Customer() {Id = 1, Name="Sally", Amount=12345},
+            new Customer() {Id = 2, Name="Billy", Amount=54632},
+            new Customer() {Id = 3, Name="John", Amount=54651},
+        };
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer List";
+            ViewBag.CustomerCount = customers.Count;
+            ViewBag.CustomerList = customers;
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            ViewData["Message"] = "Customer List";
+            ViewData["CustomerCount"] = customers.Count();
+            ViewData["CustomerList"] = customers;
+            return View();
+        }
+    }
+}
